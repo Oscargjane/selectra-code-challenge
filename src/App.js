178dcc2597ab8b2a12d755dac2c6690c7f49c7f5
Oscar Jané <oscar.gjane@gmail.com>
2022-09-02 13:15:00 +0200

    feat: add a static version of the subscription form
    It includes:
    * Accessible form component
    * Main App.js component updated to render the form in UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Theme from './components/styles/Theme';
+import GlobalStyles from './components/styles/Global';
 import { Container } from './components/styles/Container.styled';
 import Header from './components/Header';
-import GlobalStyles from './components/styles/Global';
+import SubscriptionForm from './components/SubscriptionForm';
+import UserList from './components/Users/UserList';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Container>
         <GlobalStyles />
         <Header />
+        <SubscriptionForm />
       </Container>
     </Theme>
   );
