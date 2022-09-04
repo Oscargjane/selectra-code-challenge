83a5bcfad54356c3385db45dcc138420ab9c3149
Oscar Jané <oscar.gjane@gmail.com>
2022-09-04 13:29:49 +0200

    style: add conditional styles in the form and submit button.
    It includes:
    * Conditional styles in the form component to handle the error behavior of the various input fields
    * Conditional styles in the button component to disable and enable the form submission

--- src/components/styles/Button.styled.js
+++ src/components/styles/Button.styled.js
@@ -7,11 +7,22 @@ export const Button = styled.button`
   font-weight: 600;
   border: 0;
   border-radius: 0.5rem;
-  color: ${({ theme }) => theme.colors.gray_500};
-  background-color: ${({ theme }) => theme.colors.gray_300};
+  color: ${({ theme }) => theme.colors.gray_50};
+  background-color: ${({ theme }) => theme.colors.primary_500};
   cursor: pointer;
+  transition-property: color, background-color;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 250ms;
+
+  &:disabled,
+  &:disabled:hover,
+  &:disabled:active {
+    color: ${({ theme }) => theme.colors.gray_500};
+    background-color: ${({ theme }) => theme.colors.gray_300};
+    cursor: not-allowed;
+  }
 
   @media only screen and (min-width: ${({ theme }) => theme.bp.small}) {
     justify-self: center;
-  } ;
+  }
 `;
--- src/components/styles/UserForm.styled.js
+++ src/components/styles/UserForm.styled.js
@@ -9,12 +9,12 @@ export const StyledForm = styled.form`
 export const Label = styled.label`
   display: flex;
   flex-direction: column;
-  cursor: pointer;
 
   span {
     padding-bottom: 0.8rem;
     font-size: 1.6rem;
     color: ${({ theme }) => theme.colors.gray_700};
+    cursor: pointer;
 
     i {
       margin-left: 0.4rem;
@@ -27,13 +27,25 @@ export const Input = styled.input`
   padding: 1.2rem 0.8rem;
   font-size: 1.6rem;
   border: 0;
-  border: 1px solid ${({ theme }) => theme.colors.gray_300};
+  border: 1px solid ${props => (props.invalid ? '#EF4444' : '#CFD4D8')};
   border-radius: 0.5rem;
   color: ${({ theme }) => theme.colors.gray_900};
+  background-color: ${props => (props.invalid ? '#FEF2F2' : '#F9FAFA')};
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${props => (props.invalid ? '#EF4444' : '#135EA9')};
+  }
 
   &::placeholder {
     font-size: 1.5rem;
     line-height: 0;
     color: ${({ theme }) => theme.colors.gray_500};
   }
+`;
+
+export const AlertMessage = styled.p`
+  margin-top: 1.6rem;
+  font-size: 1.6rem;
+  color: ${({ theme }) => theme.colors.alert_300};
 `;
